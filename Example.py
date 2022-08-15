a9de50e90cdbed4df7761fc679162844e7cdd69a
TradeViaPython <111175426+TradeViaPython@users.noreply.github.com>
2022-08-15 15:35:39 +0530

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,13 +1,16 @@
+
+########################################################################
+api_key = "___"         # Get from https://a3.aliceblueonline.com      After Login go to "Apps" section and create API
+user_id = "___"
+
+# Login
 from AliceBlue import Alice
 
-
-api_key = "7h7Ar8FRFoVqUSH9l5vP6Co8icUAg6cocfxDpn7RXtXJzHj7soeiILhzDLXZUdzYcIiH5mMqRGzggy3QZ6VQoDxkIaCVgR4tj1tfDOHV6iOkYPjQmQLgxUDEQ0WN4qsn"
-user_id = "273666"
-
-# Login 
 alice = Alice(user_id=user_id, api_key=api_key)
-alice.get_session_id()
+print(alice.create_session())           # Must "log in" to Alice platform before create session
+alice.get_master_contract()
 
+########################################################################
 # Basic API Calls
 print(alice.get_profile())
 print(alice.get_balance())
@@ -17,114 +20,119 @@ print(alice.get_positions(alice.POSITION_DAYWISE))
 print(alice.get_positions(alice.POSITION_NETWISE))
 print(alice.get_holdings())
 
-# Download master contract df
+#######################################################################
+# Get Master Contract
 print(alice.get_master_contract())
 
-# Get specific exchange data from master contract
+# Get specific exchange
 print(alice.get_master_contract("NSE"))
-
-# Get specific instrument tuple from master contract
-print(alice.get_instrument_by_symbol("NSE", "NIFTY 50"))
-print(alice.get_instrument_by_symbol("NSE", "UPL-EQ"))
+print(alice.get_master_contract("NFO"))
+print(alice.get_master_contract("MCX"))   # BSE, CDS, BFO also available
 
 
-# Scrip Detail
-print(alice.get_scrip_details(alice.get_instrument_by_symbol("NSE", "UPL-EQ")))
+import datetime
+# Get Instrument
+print(alice.get_instrument_by_symbol("NSE", "NIFTY BANK"))                  # NSE Indices NIFTY BANK
+print(alice.get_instrument_by_symbol("NSE", "ACC-EQ"))                      # NSE Equity symbol
+print(alice.get_instrument_by_symbol("NFO", "NIFTY22AUGFUT"))               # NFO Future symbol
+print(alice.get_instrument_by_token("NSE", "3499"))
+print(alice.get_instrument_for_fno("NFO", "BANKNIFTY", is_fut=True, expiry_date=datetime.date(2022, 8, 25)))
+print(alice.get_instrument_for_fno("NFO", "BANKNIFTY", is_fut=False, expiry_date=datetime.date(2022, 8, 18), strike=39000, is_CE=True))
 
 
+#########################################################################
 # Place Order
-order = alice.place_order(transaction_type=alice.TRANSACTION_TYPE_BUY,
-                          instrument=alice.get_instrument_by_symbol("NSE", "RELIANCE-EQ"),
-                          quantity=5,
+order = alice.place_order(transaction_type=alice.TRANSACTION_TYPE_SELL,
+                          instrument=alice.get_instrument_by_symbol("NFO", "NIFTY22AUGFUT"),
+                          quantity=100,
                           price_type=alice.PRICE_TYPE_LIMIT,
-                          product_type=alice.PRODUCT_TYPE_CNC,
-                          price=33.0,
+                          product_type=alice.PRODUCT_TYPE_NRML,
+                          price=17750,
                           trigger_price=None,
                           stop_loss=None,
                           square_off=None,
                           trailing_sl=None,
+                          validity=alice.VALIDITY_IOC,
                           complexty=alice.COMPLEXTY_REGULAR,
-                          validity=alice.VALIDITY_DAY
                           order_tag="TradeViaPython")
 print(order)
 
-
-
-# Place Bracket Order
-order = alice.place_order(transaction_type=alice.TRANSACTION_TYPE_BUY,
-                          instrument=alice.get_instrument_by_symbol("NSE", "ACC-EQ"),
-                          quantity=5,
-                          price_type=alice.PRICE_TYPE_LIMIT,
-                          product_type=alice.PRODUCT_TYPE_MIS,
-                          price=33.0,
-                          trigger_price=None,
-                          stop_loss=32,
-                          square_off=37,
-                          trailing_sl=None,
-                          complexty=alice.COMPLEXTY_BO,
-                          order_tag="TradeViaPython")
-print(order)
-
-
-
+###########################################################################
 # Modify Order
 order = alice.modify_order(nestOrderNumber="22081300001897",
                            transaction_type=alice.TRANSACTION_TYPE_BUY,
-                           instrument=alice.get_instrument_by_symbol("NFO", "NIFTY22AUGFUT"),
-                           quantity=50,
+                           instrument=alice.get_instrument_by_symbol("NSE", "ACC-EQ"),
+                           quantity=10,
                            price_type=alice.PRICE_TYPE_LIMIT,
-                           product_type=alice.PRODUCT_TYPE_NRML,
-                           price=17800.0,
+                           product_type=alice.PRODUCT_TYPE_CNC,
+                           price=2227,
                            trigger_price=None)
+
 print(order)
 
-
+#############################################################################
 # Cancel Order
 order = alice.cancel_order(nestOrderNumber="220813000001897",
                            instrument=alice.get_instrument_by_symbol("NFO", "NIFTY22AUGFUT"))
+
 print(order)
 
+#############################################################################
+# Place Bracket Order
+order = alice.place_order(transaction_type=alice.TRANSACTION_TYPE_BUY,
+                          instrument=alice.get_instrument_by_symbol("NSE", "RELIANCE-EQ"),
+                          quantity=5,
+                          price_type=alice.PRICE_TYPE_LIMIT,
+                          product_type=alice.PRODUCT_TYPE_MIS,
+                          price=2633.0,
+                          trigger_price=None,
+                          stop_loss=2620,
+                          square_off=None,
+                          trailing_sl=None,
+                          validity=alice.VALIDITY_DAY,
+                          complexty=alice.COMPLEXTY_BO,      # COMPLEXTY.AMO  for AMO order
+                          order_tag="TradeViaPython")
+print(order)
+
+################################################################################
+# WEBSOCKET
+import time
+
+socket_opened = False
 
 
+def event_handler_quote_update(message):
+    print(message)
 
 
-
-
-print(instrument)
-print(alice.get_scrip_details(instrument))
-
-
-#WEBSOCKET
-def start_websocket():
+def open_callback():
     global socket_opened
-    socket_opened = False
-    live_data = {}
-    keys_values = {"ltp": "lp", "close": "c", "open": "o", "high": "h", "low": "l", "volume": "v", "atp": "ap",  "best_bid_price": "bp1", "best_ask_price": "sp1", "oi": "toi"}
+    socket_opened = True
 
-    def event_handler_quote_update(message):
-        global live_data
-        if f"{message['e']}:{message['ts']}" not in list(live_data.keys()):
-            live_data[f"{message['e']}:{message['ts']}"] = {}
-        for k, v in keys_values.items():
-            try:
-                live_data[f"{message['e']}:{message['ts']}"][k] = message[v]
-            except:
-                live_data[f"{message['e']}:{message['ts']}"][k] = 0
-        print(message)
 
-    def open_callback():
-        global socket_opened
-        socket_opened = True
+alice.invalidate_socket_session()
+alice.create_socket_session()
+alice.start_websocket(subscribe_callback=event_handler_quote_update,
+                      socket_open_callback=open_callback,
+                      run_in_background=True)
+while not socket_opened:
+    pass
+print("Websocket : Connected")
+alice.subscribe([alice.get_instrument_by_symbol("NSE", i) for i in ["ACC-EQ", "RELIANCE-EQ", "UPL-EQ", "LUPIN-EQ"]])
+time.sleep(30)
+alice.unsubscribe([alice.get_instrument_by_symbol("NSE", i) for i in ["ACC-EQ", "RELIANCE-EQ"]])
+time.sleep(10)
 
-    alice.invalidate_socket_session()
-    alice.create_socket_session()
-    alice.start_websocket(subscribe_callback=event_handler_quote_update,
-                          socket_open_callback=open_callback)
-    while not socket_opened:
-        pass
-    print("Websocket : Connected")
-    alice.subscribe([alice.get_instrument_by_symbol("NSE", i) for i in ["ACC-EQ", "RELIANCE-EQ", "UPL-EQ", "LUPIN-EQ"]])
-    time.sleep(30)
-    alice.unsubscribe([alice.get_instrument_by_symbol("NSE", i) for i in ["ACC-EQ", "RELIANCE-EQ"]])
 
-start_websocket()
+####################################################################################
+# Get Historical Data
+import datetime
+
+instrument = alice.get_instrument_by_symbol("NSE", "LUPIN-EQ")
+from_datetime = datetime.datetime.now() - datetime.timedelta(days=7)     # From last & days
+to_datetime = datetime.datetime.now()                                    # To now
+interval = "1"       # ["1", "2", "3", "4", "5", "10", "15", "30", "60", "120", "180", "240", "D", "1W", "1M"]
+indices = False      # For Getting index data
+print(alice.get_historical(instrument, from_datetime, to_datetime, interval, indices))
+
+
